75c4a82918c3caac8b98c86acea4a1b0aa2828a6
Joby Elliott <joby@byjoby.com>
2015-10-30 08:54:30 -0600

    Fixed tests

--- tests/glue/CRUDder/CRUDderTest.php
+++ tests/glue/CRUDder/CRUDderTest.php
@@ -17,8 +17,6 @@
   * with this program; if not, write to the Free Software Foundation, Inc.,
   * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-namespace glue\Tests;
-
 use \glue\CRUDder\CRUDder;
 
 class CRUDderTest extends PHPUnit_Extensions_Database_TestCase
